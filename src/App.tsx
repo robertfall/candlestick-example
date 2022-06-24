84058756703775790e024de6dd83c7891e298575
Robert Herbst <c_rherbst@godaddy.com>
2022-06-24 14:46:55 +0200

    It's working!

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import React, { useCallback, useEffect, useState } from "react";
-import { Box, Container, Grid, styled } from "@mui/material";
+import { Box, Container } from "@mui/material";
 import { useCandlestickChart } from "./use-stock-candlestick-chart";
-import { Layer, Rect, Stage } from "react-konva";
+import { Layer, Stage } from "react-konva";
 import { CandleStick } from "./candlestick";
-import { testCandleSticks } from "./test-candlesticks";
 import { useStockWebsocket } from "./use-stock-websocket";
 import { Chart } from "./chart";
 
@@ -12,9 +10,9 @@ function App() {
   // data should be an array of all candleSticks objects we want to show in the
   // screen at every render
   // const { data: candleSticks, refresh } = useCandlestickChart("1m");
-  const { data: candleSticks, refresh } = useCandlestickChart("1m");
+  const { data: candleSticks, refresh } = useCandlestickChart("2m");
   // Uncomment this line and comment out line 17 to start receiving live events
-  // useStockWebsocket({ refresh });
+  useStockWebsocket({ refresh });
 
   return (
     <Container>
@@ -26,8 +24,8 @@ function App() {
       >
         <Stage width={600} height={400}>
           <Layer>
-            {testCandleSticks.map((candleStick, i) => (
-              <CandleStick x={i * 30} candleData={candleStick} />
+            {candleSticks.map((candleStick, i) => (
+              <CandleStick key={i} x={i * 30} candleData={candleStick} />
             ))}
           </Layer>
         </Stage>
---
+++ src/constants.tsx
@@ -0,0 +1,4 @@
+
+export const SECOND = 1000;
+export const MINUTE = 60 * SECOND;
+export const NUMBER_OF_BUCKETS = 15;
